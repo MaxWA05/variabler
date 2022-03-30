1a8c4caf53e363c5e5bad3fc5452ce3baf5a8118
MaxWA05 <max.asheim@gmail.com>
2022-03-30 08:51:34 +0200

    ¨brom¨

---
+++ main.js
@@ -0,0 +1,29 @@
+//oppgave 1
+var x;
+//deklorasjon av variabel er når du gir variabelen et spesielt navn for å identifisere den spesifike variabelen.
+
+//oppgave 2
+x = 1;
+//Å initialisere er å gi variabelen en verdi.
+//Som f.eks i koden under oppgave 2 hvor verdien av "X = 2".
+
+//oppgave 3 
+var y = 1;
+//Man kan deklarere og initialisere variabel i samme setning
+//Altså istedet for å skrive deklorasjonen først også initialiseringen i en ny linje kan man gjøre det i samme setning på samme linje.
+
+
+//oppgave 4
+let meh = 'annet';
+var noe = 'annet';
+//Forskjellen på let og var er at let er "block scope" og det funker ikke hvis du prøver å kalle på den utenfor scopet den ble skrevet.
+//Var kan bli kalt på igjen utenfor scopet den ble skrevet i mens let kan ikke.
+
+//oppgave 5
+const noen = a;
+let neon = b;
+//forskjellen på const og let er at const kan ikke endres etter den har blitt skrevet, det er litt i navnet "konstant".
+
+
+//oppgave 6 
+//Noen datatyper er string, number og object.
